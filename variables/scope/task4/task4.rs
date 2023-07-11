72c2346ed22fc7ca2a32312e207fa092b256b33a
Jose Valverde <hey@josevalverde.me>
2023-07-11 11:10:54 +0100

    task4

---
+++ variables/scope/task4/task4.rs
@@ -0,0 +1,19 @@
+// 4. Fix the error with the use of define_x
+/*
+fn main() {
+    println!("{}, world", x); 
+}
+
+fn define_x() {
+    let x = "hello";
+}
+*/
+
+fn main() {
+    define_x();
+}
+
+fn define_x() {
+    let x = "hello";
+    println!("{}, world", x); 
+}
